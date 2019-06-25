6d1b7b0ffb3d44ca81a43185a3161b77afddd679
Alister McKinley <alpatine@gmail.com>
2019-06-25 15:09:01 +1000

    Three greetings demo for props

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import { AppRegistry, Image } from 'react-native';
+import { AppRegistry, Text, View } from 'react-native';
 
-export default class Bananas extends Component {
+class Greeting extends Component {
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
     return (
-      <Image source={pic} style={{width: 193, height: 110}}/>
+      <View style={{alignItems:"center"}}>
+        <Text>Hello {this.props.name}!</Text>
+      </View>
     );
   }
 }
 
-AppRegistry.registerComponent('AwesomeProject', () => Bananas);
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems:"center", top: 50}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => LotsOfGreetings);
